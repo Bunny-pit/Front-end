d2dcc10698c5289ac8d89d5f895af2dfb29a82a8
ryuiseo <rks85227@gmail.com>
2023-07-03 23:22:17 +0900

    Feat : global style

--- src/index.tsx
+++ src/index.tsx
@@ -4,19 +4,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/Theme';
+import GlobalStyles from './styles/GlobalStyles';
+import GlobalFont from './styles/GlobalFont';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
-root.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <GlobalFont />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,33 +1,35 @@
 import React from 'react';
 import MainLogo from '../../assets/icons/MainLogo.png';
-
+import { FirstSection, Container } from './MainStyle';
 const MainPage = () => {
   return (
     <>
-      <div>
-        <header>
-          <img src={MainLogo} alt="main-logo" />
-          <button>로그인</button>
-          <button>회원가입</button>
-        </header>
-        <div>
+      <Container>
+        <FirstSection>
+          <header>
+            <img src={MainLogo} alt="main-logo" />
+            <button>로그인</button>
+            <button>회원가입</button>
+          </header>
           <div>
-            <p>친구들과 함께 만드는,</p>
-            <p>우리의 공간</p>
+            <div>
+              <p>친구들과 함께 만드는,</p>
+              <p>우리의 공간</p>
+            </div>
+            <div>
+              <p>
+                자유롭게 사진을 올리고, 공유할 수 있는 우리만의 공간을 버니톡과
+                함께 만들어가요
+              </p>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <img src="" alt="" />
+              <img src="" alt="" />
+            </div>
           </div>
-          <div>
-            <p>
-              자유롭게 사진을 올리고, 공유할 수 있는 우리만의 공간을 버니톡과
-              함께 만들어가요
-            </p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-          </div>
-        </div>
-      </div>
+        </FirstSection>
+      </Container>
     </>
   );
 };
