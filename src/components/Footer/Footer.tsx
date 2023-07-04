0b936a590f93f11a779e868b4f332590f83e1e31
Jonguk Lee <looke2930@gmail.com>
2023-07-04 15:34:51 +0900

    FIX : Footer styled-component로 수정

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,42 +1,55 @@
-import { FooterStyle } from "./FooterStyle";
 import FooterLogo from "../../assets/icons/FooterLogo.png";
 import FooterVector from "../../assets/icons/FooterVector.png";
+import {
+  FooterStyle,
+  Wrapper,
+  Sec1,
+  Logo,
+  Line,
+  Sec2,
+  Title,
+  MemberList,
+  MemberLink,
+  List,
+  ProjectLink,
+} from "./FooterStyle";
 
-type HeaderProps = {
-  text: string;
-};
 const DefaultFooter = () => {
   return (
     <FooterStyle>
-      <div className="wrapper">
-        <div className="sec1">
-          <img className="logo" src={FooterLogo} alt="푸터 로고" />
-          <img className="vector" src={FooterVector} alt="선!" />
-        </div>
-        <div className="sec2">
-          <p>친구들과 함께 만드는 우리의 공간</p>
-          <ul>
-            <li>
-              <a href="https://github.com/PossumCircus">김종현</a>
-            </li>
-            <li>
-              <a href="https://github.com/ryuiseo">류이서</a>
-            </li>
-            <li>
-              <a href="https://github.com/ddun-Ttu">이준미</a>
-            </li>
-            <li>
-              <a href="https://github.com/Loo-ke">이종욱</a>
-            </li>
-            <li>
-              <a href="https://github.com/ChanghyeonO">오창현</a>
-            </li>
-          </ul>
-          <a href="https://github.com/Bunny-pit/bunny-pit">
+      <Wrapper>
+        <Sec1 className="sec1">
+          <Logo className="logo" src={FooterLogo} alt="푸터 로고" />
+          <Line className="vector" src={FooterVector} alt="선!" />
+        </Sec1>
+        <Sec2 className="sec2">
+          <Title>친구들과 함께 만드는 우리의 공간</Title>
+          <MemberList>
+            <List>
+              <MemberLink href="https://github.com/PossumCircus">
+                김종현
+              </MemberLink>
+            </List>
+            <List>
+              <MemberLink href="https://github.com/ryuiseo">류이서</MemberLink>
+            </List>
+            <List>
+              <MemberLink href="https://github.com/ddun-Ttu">이준미</MemberLink>
+            </List>
+            <List>
+              <MemberLink href="https://github.com/Loo-ke">이종욱</MemberLink>
+            </List>
+            <List>
+              <MemberLink href="https://github.com/ChanghyeonO">
+                오창현
+              </MemberLink>
+            </List>
+          </MemberList>
+          <ProjectLink href="https://github.com/Bunny-pit/bunny-pit">
             https://github.com/Bunny-pit/bunny-pit
-          </a>
-        </div>
-      </div>
+          </ProjectLink>
+        </Sec2>
+      </Wrapper>
     </FooterStyle>
   );
 };
