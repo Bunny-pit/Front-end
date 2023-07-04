9fa628e6d37e518e827e9d884141c1c6af41c896
Jonguk Lee <looke2930@gmail.com>
2023-07-04 23:26:38 +0900

    pull dev branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/Main/Main';
-import UserMainPage from './pages/UserMain/UserMain';
+import UserMain from './pages/UserMain/UserMain';
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/usermain" element={<UserMainPage />} />
+          <Route path="/usermain" element={<UserMain />} />
         </Routes>
       </Router>
     </>
