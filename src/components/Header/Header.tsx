eca086941a5b5bc731e0c6da13f39481aec53bd1
Jonguk Lee <looke2930@gmail.com>
2023-07-04 15:41:18 +0900

    FIX : Header styled-component로 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,33 @@
-import { HedearStyle } from "./HeaderStyle";
+import {
+  HedearStyle,
+  Logo,
+  HeaderWrapper,
+  MenuUl,
+  MenuLi,
+  MenuLink,
+  Option,
+} from "./HeaderStyle";
 import HeaderLogo from "../../assets/icons/HedearLogo.png";
 import HeaderOption from "../../assets/icons/HedearOption.png";
-type HeaderProps = {
-  text: string;
-};
+
 const DefaultHeader = () => {
   return (
     <HedearStyle>
-      <div className="wrapper">
-        <img className="logo" src={HeaderLogo} />
-        <ul>
-          <li>
-            <a href="#">익명의 버니들</a>
-          </li>
-          <li>
-            <a href="#">친구 버니들</a>
-          </li>
-          <li>
-            <a href="#">버니톡</a>
-          </li>
-        </ul>
-        <img className="option" src={HeaderOption} />
-      </div>
+      <HeaderWrapper>
+        <Logo src={HeaderLogo} alt="로고" />
+        <MenuUl>
+          <MenuLi>
+            <MenuLink href="#">익명의 버니들</MenuLink>
+          </MenuLi>
+          <MenuLi>
+            <MenuLink href="#">친구 버니들</MenuLink>
+          </MenuLi>
+          <MenuLi>
+            <MenuLink href="#">버니톡</MenuLink>
+          </MenuLi>
+        </MenuUl>
+        <Option src={HeaderOption} alt="옵션" />
+      </HeaderWrapper>
     </HedearStyle>
   );
 };
