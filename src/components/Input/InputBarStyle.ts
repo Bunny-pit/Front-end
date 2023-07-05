7b62785150a5eb4f243b2a48ae4bca8163890ff2
ryuiseo <rks85227@gmail.com>
2023-07-05 11:30:19 +0900

    Merge : FE_dev_main

--- src/components/Input/InputBarStyle.ts
+++ src/components/Input/InputBarStyle.ts
@@ -2,4 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div``;
 export const InputArea = styled.input``;
-export const SendIcon = styled.img``;
+export const SendIcon = styled.img`
+  width: 20%;
+`;
