772873aed7a567a4b14136fddf71bc3d0c646ec6
Jonguk Lee <looke2930@gmail.com>
2023-07-07 19:26:11 +0900

    FEAT : 게시글 상세 페이지 디자인 1차 작업

--- src/pages/UserMain/Detail/Detail.tsx
+++ src/pages/UserMain/Detail/Detail.tsx
@@ -1,43 +1,76 @@
 import React from 'react';
 import DetailHeader from '../../../components/Header/DetailHeader/DetailHeader';
-import ProfileImg from '../../../assets/images/userimage.png';
-import { Container } from './DetailStyle';
+import Footer from '../../../components/Footer/Footer';
+import ProfileImg from '../../../assets/images/userimagesmall.png';
+import DeleteIcon from '../../../assets/icons/DeleteIcon.png';
+import likeIcon from '../../../assets/icons/like.png';
+import { Container, DeleteButton } from './DetailStyle';
 
 const Detail = () => {
 	return (
 		<>
 			<DetailHeader />
 			<Container>
-				<div>
-					<div>
+				<div className='profile-wrap'>
+					<div className='profile'>
 						<img src={ProfileImg} alt='userImg' />
 						<p>유저 아이디</p>
 					</div>
-					<div>
-						<button>삭제버튼</button>
+					<div className='deletebutton-wrap'>
+						<button>
+							<DeleteButton src={DeleteIcon} alt='삭제버튼' />
+						</button>
 					</div>
 				</div>
-				<div>
+				<div className='post-wrap'>
 					{/* 게시글 큰 wrap */}
-					<div>
+					<div className='img-wrap'>
 						{/* 사진 wrap */}
-						<p>이미지 들어갈 자리</p>
+						<p className='post-img'>이미지 들어갈 자리</p>
 					</div>
-					<div>
+					<div className='post-detail-wrap'>
 						{/* 좋아요버튼, 좋아요수, 시간 */}
-						<button>좋아요 버튼</button>
-						<p>
-							좋아요 수 <span>0</span>
-						</p>
-						<p>시간 들어갈 자리</p>
+						<div className='like-wrap'>
+							<button className='like-button'>
+								<img src={likeIcon} alt='좋아요 버튼' />
+							</button>
+							<p className='like-count-wrap'>
+								좋아요 수 <span className='like-count'>0</span>
+							</p>
+						</div>
+						<p className='post-time'>시간 들어갈 자리</p>
 					</div>
-					<div>
+					<div className='content-wrap'>
 						{/* 게시글 내용 들어갈 자리 */}
-						<div>게시글 내용이 들어갈 자리입니다아아아아아아아앙</div>
+						<div className='content'>
+							게시글 내용이 들어갈 자리입니다아아아아아아아앙
+						</div>
 					</div>
 					<hr />
 				</div>
+				<div className='comment-wrap'>
+					<h3>댓글</h3>
+					<ul className='comment-ul'>
+						<li className='comment-li'>
+							<h4 className='comment-userId'>UserName</h4>
+							<p className='comment-content'>댓글 내용이다 깽깽이들아!!!</p>
+						</li>
+						<li className='comment-li'>
+							<h4 className='comment-userId'>UserName</h4>
+							<p className='comment-content'>댓글 내용이다 깽깽이들아!!!</p>
+						</li>
+						<li className='comment-li'>
+							<h4 className='comment-userId'>UserName</h4>
+							<p className='comment-content'>댓글 내용이다 깽깽이들아!!!</p>
+						</li>
+					</ul>
+					<input
+						type='text'
+						className='comment-input'
+						placeholder='댓글 남기기'></input>
+				</div>
 			</Container>
+			<Footer />
 		</>
 	);
 };
--- src/pages/UserMain/Detail/DetailStyle.ts
+++ src/pages/UserMain/Detail/DetailStyle.ts
@@ -1,7 +1,101 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-	border: 1px solid black;
 	width: 75%;
+	margin: 0 auto;
+	.profile-wrap {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		.profile {
+			display: flex;
+			align-items: center;
+			img {
+				width: 10rem;
+			}
+			p {
+				font-size: 2.7rem;
+			}
+		} //profile
+		button {
+			border: none;
+			background: #fff;
+			cursor: pointer;
+		}
+	} // profile-wrap
+	.post-wrap {
+		.img-wrap {
+			.post-img {
+				width: 100%;
+			} //post-img
+			width: 100%;
+			height: 60rem;
+			background: lightblue;
+		} //img-wrap
+		.post-detail-wrap {
+			display: flex;
+			flex-direction: row;
+			justify-content: space-between;
+			align-items: center;
+			.like-wrap {
+				display: flex;
+				align-items: center;
+				.like-button {
+					border: none;
+					background: #fff;
+					img {
+						width: 3.6rem;
+					} //img
+				}
+			} //like-button
+			.like-count-wrap {
+				font-size: 2rem;
+				.like-count {
+					font-weight: 600;
+				} //like-count
+			} //like-wrap
+			.post-time {
+				font-size: 1.8rem;
+				color: ${({ theme }) => theme.colors.gray600};
+			}
+		} //.post-detail-wrap
+		.content-wrap {
+			margin: 2rem 0;
+			font-size: 2rem;
+		} //content-wrap
+	} //post-wrap
+	.comment-wrap {
+		h3 {
+			text-align: center;
+		}
+		.comment-ul {
+			list-style: none;
+			padding: 0;
+			display: flex;
+			flex-direction: column;
+			.comment-li {
+				display: flex;
+				flex-direction: row;
+				align-items: center;
+				.comment-userId {
+					font-size: 1.8rem;
+					margin-right: 2rem;
+				}
+				.comment-content {
+					font-size: 1.8rem;
+				}
+			} //comment-li
+		} //comment-ul
+		.comment-input {
+			width: 99%;
+			margin: 0;
+			padding: 0;
+			margin: 2rem 0 6rem;
+		}
+	} //comment-wrap
+`;
+export const DeleteButton = styled.img`
+	width: 3.3rem;
+	background: #fff;
 	margin: 0 auto;
 `;
