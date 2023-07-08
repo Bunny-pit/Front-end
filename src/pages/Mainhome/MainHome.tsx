9a2aa9c6932e4427a1f408831009efc0f3802aac
ChanghyeonO <ckdgus5189@gmail.com>
2023-07-08 11:32:22 +0900

    feat: MainhomeStyle.ts에 Title css완료

--- src/pages/Mainhome/MainHome.tsx
+++ src/pages/Mainhome/MainHome.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
-// import {} from './MainHomeStyle';
+import userImage from '../../assets/images/userimage.png';
+import {
+	Container,
+	Title,
+	ContentBox,
+	ImageWrap,
+	UserImage,
+} from './MainHomeStyle';
 
 const Mainhome = () => {
 	return (
 		<>
 			<Header />
+			<Container>
+				<Title>
+					<h1>Unknown Bunnies</h1>
+				</Title>
+				<ContentBox>
+					<ImageWrap>
+						<UserImage src={userImage} alt='User Image' />
+					</ImageWrap>
+				</ContentBox>
+			</Container>
 			<Footer />
 		</>
 	);
--- src/pages/Mainhome/MainHomeStyle.ts
+++ src/pages/Mainhome/MainHomeStyle.ts
@@ -1,1 +1,23 @@
-export {};
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	width: 100%;
+	height: 500rem;
+`;
+
+export const Title = styled.div`
+	width: 100%;
+	margin-top: 15rem;
+	text-align: center;
+	font-size: 2rem;
+	font-weight: bold;
+	color: #db7bf9;
+`;
+
+export const ContentBox = styled.div`
+	width: 100%;
+	margin: 20rem;
+`;
+export const ImageWrap = styled.div``;
+
+export const UserImage = styled.img``;
