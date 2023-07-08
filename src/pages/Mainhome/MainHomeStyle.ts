82f9f590ba14f9d361e2460168e68f589531d2b4
ChanghyeonO <ckdgus5189@gmail.com>
2023-07-08 13:21:15 +0900

    ContentBox 틀 완성, CSS 구현

--- src/pages/Mainhome/MainHomeStyle.ts
+++ src/pages/Mainhome/MainHomeStyle.ts
@@ -2,22 +2,59 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 	width: 100%;
-	height: 500rem;
+	height: 100rem;
 `;
 
 export const Title = styled.div`
 	width: 100%;
-	margin-top: 15rem;
+	margin: 15rem 0 15rem 0;
 	text-align: center;
-	font-size: 2rem;
-	font-weight: bold;
+	font-size: 5rem;
+	font-weight: 700;
 	color: #db7bf9;
 `;
 
 export const ContentBox = styled.div`
 	width: 100%;
-	margin: 20rem;
+	display: flex;
+	padding: 0 22%;
+	margin: 0 auto;
+	box-sizing: border-box;
 `;
-export const ImageWrap = styled.div``;
+export const ImageWrap = styled.div`
+	margin: 5rem auto;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
 
-export const UserImage = styled.img``;
+export const UserImage = styled.img`
+	width: 100%;
+	margin: 0 auto;
+`;
+export const InnerContent = styled.div`
+	position: relative;
+	padding: 5%;
+`;
+
+export const UserSecretName = styled.h2`
+	font-size: 2.5rem;
+`;
+
+export const Content = styled.p`
+	font-size: 1.8rem;
+`;
+
+export const Date = styled.p`
+	font-size: 1.5rem;
+	color: #717171;
+`;
+
+export const Wrapper = styled.div`
+	position: absolute;
+	bottom: 0;
+	right: 0;
+`;
+
+export const Edit = styled.button``;
+export const Delete = styled.button``;
