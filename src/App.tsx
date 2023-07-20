6c8f0ecdf4bb2a4c921df69e6aebc9c2d892f42f
Jonguk Lee <looke2930@gmail.com>
2023-07-20 15:37:31 +0900

    FIX : app.tsx에 /post/upload  경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import UserMainPage from './pages/UserMain/UserMain';
 import Chatting from './pages/Chatting/Chatting';
 import Detail from './pages/UserMain/Detail/Detail';
 import LoginPage from './pages/UserAccount/Login/Login';
+import UploadPost from './pages/UserMain/UploadPost/UploadPost';
 // import SignUpPage from './pages/SignUp/SignUp'
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
 					<Route path='/login' element={<LoginPage />} />
 					<Route path='/post' element={<UserMainPage />} />
 					<Route path='/post/:postId' element={<Detail />} />
+					<Route path='/post/upload' element={<UploadPost />} />
 					<Route path='/mainhome' element={<Mainhome />} />
 					<Route path='/chatting/*' element={<Chatting />} />
 				</Routes>
