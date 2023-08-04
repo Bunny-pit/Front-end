ff6c678117153adfe233578a80074a01c0812b6e
ChanghyeonO <ckdgus5189@gmail.com>
2023-08-04 15:17:32 +0900

    feat: SendBox 컴포넌트 세분화

--- src/components/MainHomeSendBox/MainHomeSendBox.tsx
+++ src/components/MainHomeSendBox/MainHomeSendBox.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import sendIcon from '../../assets/icons/Sendicon.png';
 import useMainHomePost from '../../hooks/useMainHomePost';
+import MainHomeSendBoxTextArea from './MainhomeSendBoxDetail/MainhomeSendBoxTextArea';
+import MainHomeSendBoxButton from './MainhomeSendBoxDetail/MainhomeSendBoxButton';
 
-import {
-	TextBox,
-	TextWrapper,
-	TextArea,
-	SendButton,
-	SendIcon,
-} from './MainHomeSendBoxStyle';
+import { TextBox, TextWrapper } from './MainHomeSendBoxStyle';
 
 interface Props {
 	mainHomePost: ReturnType<typeof useMainHomePost>;
@@ -17,7 +12,7 @@ interface Props {
 const MainHomeSendBox = ({ mainHomePost }: Props) => {
 	const { newPostContent, setNewPostContent, createPost } = mainHomePost;
 
-	const hanleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const { value } = e.target;
 		setNewPostContent(value);
 	};
@@ -34,16 +29,13 @@ const MainHomeSendBox = ({ mainHomePost }: Props) => {
 	return (
 		<TextBox>
 			<TextWrapper>
-				<TextArea
+				<MainHomeSendBoxTextArea
 					placeholder='익명으로 글을 남기게 되면 프로필이 비공개 처리돼요!'
 					value={newPostContent}
-					onChange={hanleInputChange}
+					onChange={handleInputChange}
 					onKeyDown={handleKeyDown}
 				/>
-
-				<SendButton onClick={createPost}>
-					<SendIcon src={sendIcon} alt='Send Icon' />
-				</SendButton>
+				<MainHomeSendBoxButton createPost={createPost} />
 			</TextWrapper>
 		</TextBox>
 	);
