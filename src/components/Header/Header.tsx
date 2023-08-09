cb3a2d3a59f6410f56b5cbf0cfe154022580da37
김종현 <plmij1501@gmail.com>
2023-08-09 14:42:29 +0900

    fix : 헤더의 톱니 클릭시 마이페이지(정보수정)로 넘어감

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,9 +9,10 @@ import {
 import { Link } from 'react-router-dom';
 import HeaderLogo from '../../assets/icons/HedearLogo.png';
 import HeaderOption from '../../assets/icons/HedearOption.png';
-import handleLogout from '../../utils/logout';
+import { useNavigate } from 'react-router-dom';
 
 const DefaultHeader = () => {
+	const navigate = useNavigate();
 	return (
 		<HedearStyle>
 			<HeaderWrapper>
@@ -32,8 +33,8 @@ const DefaultHeader = () => {
 						<Link to='/friendchatting/*'>친구 버니톡</Link>
 					</MenuLi>
 				</MenuUl>
-				<Option src={HeaderOption} alt='옵션' />
-				<button onClick={() => { handleLogout() }}>로그아웃(임시)</button>
+				{/* 아래 navigate 현재 정보수정 및 탈퇴 페이지(마이 페이지 하위 기능)로 넘어감. */}
+				<Option onClick={() => { navigate('/user/edit') }} src={HeaderOption} alt='옵션' />
 			</HeaderWrapper>
 		</HedearStyle>
 	);
