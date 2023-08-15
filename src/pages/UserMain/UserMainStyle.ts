1858916998b4da18798f48644ef24167a673567f
Jonguk Lee <looke2930@gmail.com>
2023-08-15 13:00:29 +0900

    FIX : 버튼 마우스 포인터 효과 추가 / 게시글 상세 페이지 댓글 클릭 시 유저 페이지로 이동

--- src/pages/UserMain/UserMainStyle.ts
+++ src/pages/UserMain/UserMainStyle.ts
@@ -7,7 +7,7 @@ export const Container = styled.div`
 		width: 50%;
 	}
 	@media only screen and (min-width: 391px) and (max-width: 768px) {
-		width: 60%;
+		width: 70%;
 	}
 	@media only screen and (min-width: 0px) and (max-width: 390px) {
 		width: 100%;
@@ -33,10 +33,11 @@ export const UserImage = styled.img`
 	height: 31rem;
 	object-fit: cover;
 	background-color: #fff;
+	cursor: pointer;
 
 	@media only screen and (min-width: 391px) and (max-width: 768px) {
-		width: 20rem;
-		height: 20rem;
+		width: 18rem;
+		height: 18rem;
 	}
 	@media only screen and (min-width: 0px) and (max-width: 390px) {
 		width: 15rem;
